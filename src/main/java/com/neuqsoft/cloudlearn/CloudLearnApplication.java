45e94ee088c89c9b7b0dcdfb3ba1ac6e26d0aace
sunjr <sunjr@neuqsoft.com>
2020-08-03 11:42:49 +0800

    v0.0.0|init

---
+++ src/main/java/com/neuqsoft/cloudlearn/CloudLearnApplication.java
@@ -0,0 +1,13 @@
+package com.neuqsoft.cloudlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudLearnApplication.class, args);
+    }
+
+}
