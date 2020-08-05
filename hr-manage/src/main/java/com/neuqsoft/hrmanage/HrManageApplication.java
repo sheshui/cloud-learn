9a5ef4f12412ad49fa246cb275582056ebc370f8
sunjr <sunjr@neuqsoft.com>
2020-08-05 10:19:31 +0800

    v0.0.8|add:添加本机nacos服务注册

--- hr-manage/src/main/java/com/neuqsoft/hrmanage/HrManageApplication.java
+++ hr-manage/src/main/java/com/neuqsoft/hrmanage/HrManageApplication.java
@@ -2,11 +2,15 @@ package com.neuqsoft.hrmanage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author sunjiarui
  * @Date 2020/08/03
  */
+@EntityScan("com.neuqsoft.common")
+@EnableDiscoveryClient
 @SpringBootApplication
 public class HrManageApplication {
 
