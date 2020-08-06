a97c964583e8d33c76aaf1409509f0fb59ac8e27
sunjr <sunjr@neuqsoft.com>
2020-08-06 13:26:10 +0800

    v0.0.12|feat:用户认证模块

---
+++ auth-server/src/main/java/com/neuqsoft/authserver/AuthServerApplication.java
@@ -0,0 +1,16 @@
+package com.neuqsoft.authserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sunjiarui
+ */
+@SpringBootApplication
+public class AuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerApplication.class, args);
+    }
+
+}
