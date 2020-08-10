b51c18e7c166610366dbe5ac3cefbe5a5fc8034c
慑水 <sheshui1129@foxmail.com>
2020-08-11 02:50:40 +0800

    v0.0.16|fix:网关鉴权失败。

--- gateway/src/main/java/com/neuqsoft/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/neuqsoft/gateway/config/SecurityConfig.java
@@ -1,27 +1,22 @@
 package com.neuqsoft.gateway.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
 /**
  * @author sheshui
  * @date 2020/8/11
  * @description
  */
 
-@EnableWebFluxSecurity
-public class SecurityConfig {
-    @Bean
-    SecurityWebFilterChain springSevurityFilterChain(ServerHttpSecurity http) throws Exception {
-        http.authorizeExchange()
-                .pathMatchers("/api/**").hasAnyAuthority("Role_User")
-                .anyExchange().authenticated()
-                .and()
-                .oauth2ResourceServer()
-                .jwt();
-
-        return http.build();
-    }
-}
+//@EnableWebFluxSecurity
+//public class SecurityConfig {
+//    @Bean
+//    SecurityWebFilterChain springSevurityFilterChain(ServerHttpSecurity http) throws Exception {
+//        http.authorizeExchange()
+//                .pathMatchers("/api/**").hasAnyAuthority("Role_User")
+//                .anyExchange().authenticated()
+//                .and()
+//                .oauth2ResourceServer()
+//                .jwt();
+//
+//        return http.build();
+//    }
+//}
