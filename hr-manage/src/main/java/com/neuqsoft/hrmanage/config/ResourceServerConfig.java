4d017fc0c8242586027b034ddf6b7d5796dd0a78
sunjr <sunjr@neuqsoft.com>
2020-08-14 18:10:24 +0800

    v0.0.15|fix:认证服务器使用redis缓存

---
+++ hr-manage/src/main/java/com/neuqsoft/hrmanage/config/ResourceServerConfig.java
@@ -0,0 +1,64 @@
+package com.neuqsoft.hrmanage.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
+/**
+ * @author sunjiarui
+ * @Date 2020/8/13
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+    @Autowired
+    private RedisConnectionFactory factory;
+
+    /**
+     * token服务配置
+     */
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore());
+    }
+
+    /**
+     * 路由安全配置
+     *
+     * @param http 访问
+     * @throws Exception
+     */
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/**").authenticated()
+                .and().csrf().disable();
+    }
+
+    /**
+     * jwt token token校验器
+     */
+    @Bean
+    public TokenStore tokenStore() {
+        return new RedisTokenStore(factory);
+    }
+
+    /**
+     * Token转换器必须与认证服务一致
+     */
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter();
+        accessTokenConverter.setSigningKey("sheshui");
+        return accessTokenConverter;
+    }
+
+}
