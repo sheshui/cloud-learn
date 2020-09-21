1537cc72c9f1c8ceff959131c11d4e9680ba07f2
sunjr <sunjr@neuqsoft.com>
2020-09-21 17:34:35 +0800

    v0.0.18|fix:数据库登录

--- auth-server/src/main/java/com/neuqsoft/authserver/repo/UserAuthRepo.java
+++ auth-server/src/main/java/com/neuqsoft/authserver/repo/UserAuthRepo.java
@@ -11,4 +11,27 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface UserAuthRepo extends JpaRepository<UserAuth, String> {
+    /**
+     * 按照用户名查询用户
+     *
+     * @param userName 用户名
+     * @return 查询到的用户
+     */
+    UserAuth findByUserName(String userName);
+
+    /**
+     * 按照注册邮箱查询用户
+     *
+     * @param userEmail 用户邮箱
+     * @return 查询到的用户
+     */
+    UserAuth findByUserEmail(String userEmail);
+
+    /**
+     * 按照用户手机号查询用户
+     *
+     * @param userPhone 用户的手机号
+     * @return 查询到的用户
+     */
+    UserAuth findByUserPhone(String userPhone);
 }
--- auth-server/src/main/java/com/neuqsoft/authserver/service/MyUserDetailServiceImpl.java
+++ auth-server/src/main/java/com/neuqsoft/authserver/service/MyUserDetailServiceImpl.java
@@ -1,27 +1,59 @@
 package com.neuqsoft.authserver.service;
 
+import cn.hutool.core.lang.Validator;
+import com.neuqsoft.authserver.entity.UserAuth;
+import com.neuqsoft.authserver.entity.UserRole;
 import com.neuqsoft.authserver.repo.UserAuthRepo;
+import com.neuqsoft.authserver.repo.UserRoleRepo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author sunjiarui
  * @Date 2020/8/11
  */
+@Slf4j
 @Service
 public class MyUserDetailServiceImpl implements UserDetailsService {
     @Autowired
     UserAuthRepo userAuthRepo;
+    @Autowired
+    UserRoleRepo userRoleRepo;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        try{
-//            UserAuth userAuth=Validate
-//        }
+        log.info("@#@#@#@#@#" + username + "|开始认证登录@#@#@#@#@#");
+        UserAuth auth;
+        if (Validator.isEmail(username)) {
+            log.info("邮箱登录中...");
+            auth = userAuthRepo.findByUserEmail(username);
+        } else if (Validator.isMobile(username)) {
+            log.info("手机号登录中...");
+            auth = userAuthRepo.findByUserPhone(username);
+        } else {
+            log.info("用户名登录中...");
+            auth = userAuthRepo.findByUserName(username);
+        }
+        if (auth == null) {
+            throw new UsernameNotFoundException("用户不存在");
+        } else {
+            List<UserRole> roleList = userRoleRepo.findByUserId(auth.getId());
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            roleList.forEach(userRole -> {
+                authorities.add(new SimpleGrantedAuthority(userRole.getRoleId()));
+            });
 
-        return null;
+            return new User(username, auth.getUserPwd(), authorities);
+        }
     }
 }
