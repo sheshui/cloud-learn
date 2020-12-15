869af74e074f42f4988e1181af6bdc402286c2ce
sunjr <sunjr@neuqsoft.com>
2020-12-15 19:51:20 +0800

    v0.0.25|fix:用户管理调试，更改

--- auth-server/src/main/java/com/neuqsoft/authserver/service/MyUserDetailServiceImpl.java
+++ auth-server/src/main/java/com/neuqsoft/authserver/service/MyUserDetailServiceImpl.java
@@ -9,7 +9,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -55,7 +54,7 @@ public class MyUserDetailServiceImpl implements UserDetailsService {
             List<GrantedAuthority> authorities = new ArrayList<>();
             roleList.forEach(userRole -> authorities.add(new SimpleGrantedAuthority(userRole.getRoleId())));
 
-            return new User(auth.getUserId(), passwordEncoder.encode(auth.getUserPwd()), authorities);
+            return new UserDetail(auth, auth.getUserId(), passwordEncoder.encode(auth.getUserPwd()), authorities);
         }
     }
 }
---
+++ auth-server/src/main/java/com/neuqsoft/authserver/service/UserDetail.java
@@ -0,0 +1,56 @@
+package com.neuqsoft.authserver.service;
+
+import com.neuqsoft.authserver.entity.UserAuth;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Collection;
+
+/**
+ * @author sunjiarui
+ */
+@Getter
+@Setter
+public class UserDetail extends User {
+    /**
+     * 用户id
+     */
+    @Id
+    @ApiModelProperty("用户主键")
+    @GeneratedValue(generator = "jpa-uuid")
+    private String userId;
+    /**
+     * 用户名
+     */
+    @ApiModelProperty("用户名")
+    private String userName;
+    /**
+     * 用户手机号
+     */
+    @ApiModelProperty("用户手机号")
+    private String userPhone;
+    /**
+     * 用户邮箱
+     */
+    @ApiModelProperty("用户邮箱")
+    private String userEmail;
+    /**
+     * 用户状态
+     */
+    @ApiModelProperty("用户状态")
+    private String userStatus;
+
+    public UserDetail(UserAuth userAuth, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+        super(username, password, authorities);
+        this.userId = userAuth.getUserId();
+        this.userName = userAuth.getUserName();
+        this.userPhone = userAuth.getUserPhone();
+        this.userEmail = userAuth.getUserEmail();
+        this.userStatus = userAuth.getUserStatus();
+    }
+}
